0b13b39f1270ec47df68ef833223ec3edcc0b622
Sebas Nadu <sebastiannavarro.fl@gmail.com>
2024-11-11 15:40:56 +0100

    add node server for the react spa

--- srcs/requirements/bonus/website/tools/server.js
+++ srcs/requirements/bonus/website/tools/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const app = express();
 
-const app = express(express.static("public"));
+app.use(express.static("public"));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
